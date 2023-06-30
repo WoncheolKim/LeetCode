5fa62f75dac10f6031d7c8987bcd9ea44f92a15a
Woncheol Kim <98872900+WoncheolKim@users.noreply.github.com>
2023-06-29 17:26:51 -0700

    Time: 66 ms (41.59%) | Memory: 45.1 MB (9.36%) - LeetSync

---
+++ reverse-words-in-a-string/reverse-words-in-a-string.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    const ret = [];
+    let word = [];
+    for (let i =0; i < s.length; ++i) {
+        if(s.charAt(i) === ' ') {
+            word.length > 0 && ret.unshift(word.join(''));
+            word = [];
+        }
+        else {
+            word.push(s.charAt(i));
+        }
+    }
+    word.length > 0 && ret.unshift(word.join(''));
+    return ret.join(' ');
+};
